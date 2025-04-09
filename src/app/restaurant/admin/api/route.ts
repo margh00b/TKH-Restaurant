2077552d8f3e31f76e5653615630cb3ecd75fd39
Marghoob <marghoobahmd25@gmail.com>
2025-04-08 19:46:55 -0600

    Api Routes Fix #2

--- src/app/restaurant/admin/api/route.ts
+++ src/app/restaurant/admin/api/route.ts
@@ -2,37 +2,42 @@ import { NextResponse } from "next/server";
 import prisma from "../../../../../prisma/client";
 import { compare } from "bcrypt";
 import jwt from "jsonwebtoken";
+import { supabase } from "@/utils/supabaseClient";
 
 export async function POST(request: any) {
-	const { username, password } = await request.json();
-	const user = await prisma.user.findUnique({
-		where: {
-			username,
-		},
-	});
-	
-	if (user) {
-		const isVerified = await compare(password, user.password);
-		if (isVerified) {
-			let response = NextResponse.json(
-				{ message: "Logged in" },
-				{ status: 200 }
-			);
+  const { username, password } = await request.json();
+  const { data: user, error } = await supabase
+    .from("User")
+    .select("*")
+    .eq("username", username)
+    .single();
 
-			const token = jwt.sign({ username }, process.env.JWT_SECRET ?? "");
+  if (error || !user) {
+    return NextResponse.json(
+      { message: "Invalid credentials" },
+      { status: 401 }
+    );
+  }
 
-			response.cookies.set({
-				name: "token",
-				value: token,
-				httpOnly: true,
-				sameSite: "lax",
-			});
+  if (user) {
+    const isVerified = await compare(password, user.password);
+    if (isVerified) {
+      let response = NextResponse.json(
+        { message: "Logged in" },
+        { status: 200 }
+      );
 
-			return response;
-		}
-	}
-	return NextResponse.json(
-		{ message: "Invalid credentials" },
-		{ status: 401 }
-	);
+      const token = jwt.sign({ username }, process.env.JWT_SECRET ?? "");
+
+      response.cookies.set({
+        name: "token",
+        value: token,
+        httpOnly: true,
+        sameSite: "lax",
+      });
+
+      return response;
+    }
+  }
+  return NextResponse.json({ message: "Invalid credentials" }, { status: 401 });
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,18 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export const middleware = async(req: NextRequest) => {
-	const token = req.cookies.get("token")?.value;
-	console.log(req.url, !token);
-	if (req.url.includes("/orders") && !token) {
-		const response = NextResponse.redirect(new URL('restaurant/admin', req.url));
-
-		return response;
-	}
-
-	return NextResponse.next();
+export const middleware = async (req: NextRequest) => {
+  const token = req.cookies.get("token")?.value;
+  console.log(req.url, !token);
+  if (!token) {
+    if (req.nextUrl.pathname.startsWith("/api/orders")) {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+    return NextResponse.redirect(new URL("/restaurant/admin", req.url));
+  }
 };
-
 export const config = {
-	matcher: [
-		'/((?!api|_next/static|_next/image|favicon.ico).*)'
-	]
+  matcher: [
+    "/((?!api/(?!orders/).+|_next/static|_next/image|favicon.ico).*)",
+    "/api/orders/:path",
+  ],
 };
