56138ed08af8cc0ecdd9a58b5698c67d990f1fdc
Marghoob <marghoobahmd25@gmail.com>
2025-04-11 22:10:51 -0600

    Prod Test #2

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const middleware = async (req: NextRequest) => {
-  /* const token = req.cookies.get("token")?.value;
+  const token = req.cookies.get("token")?.value;
   console.log(req.url, !token);
 
   if (req.url.includes("/api/orders") && !token) {
@@ -18,7 +18,7 @@ export const middleware = async (req: NextRequest) => {
   ) {
     const loginUrl = new URL("/restaurant/admin", req.url);
     return NextResponse.redirect(loginUrl);
-  } */
+  }
 
   return NextResponse.next();
 };
