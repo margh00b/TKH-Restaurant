4610b53827e834370399c1038dced132ae60b08f
Marghoob <marghoobahmd25@gmail.com>
2025-04-11 22:33:25 -0600

    Cache Fix #4

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -2,5 +2,14 @@ import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+const customFetch = (input: RequestInfo | URL, init?: RequestInit) => {
+  return fetch(input.toString(), {
+    ...init,
+    cache: "no-store",
+  });
+};
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    fetch: customFetch,
+  },
+});
